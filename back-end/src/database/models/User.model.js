3dbce658b9a937a947841bf689722a00aa123982
João Victor Braga <joao.victtor07@hotmail.com>
2023-02-28 14:21:42 -0300

    fix: arruma relacionamentos das tabelas removendo o nome da tabela que estava no plural

--- back-end/src/database/models/User.model.js
+++ back-end/src/database/models/User.model.js
@@ -13,7 +13,7 @@ module.exports = (sequelize, DataTypes) => {
   });
   
   User.associate = (models) => {
-    User.hasMany(models.Sales,
+    User.hasMany(models.Sale,
          { foreignKey: 'userId', as: 'sales' });
      };
 
