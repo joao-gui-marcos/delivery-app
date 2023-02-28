4fded26545d1ea579745036ee7b386f67c7f7a74
João Victor Braga <joao.victtor07@hotmail.com>
2023-02-28 14:10:01 -0300

    feat: cria rota /login

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,10 @@
+const loginRouter = require('./routers/login.routes')
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
 app.get('/coffee', (_req, res) => res.status(418).end());
+app.use('/login', loginRouter);
 
 module.exports = app;
