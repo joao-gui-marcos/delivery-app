8f54a7f60c91fc85a63900a69b85aee0d520a5d7
Joao Guilherme <jguilherme.marcos@gmail.com>
2023-03-01 16:24:05 -0300

    feat: implement customer navBar component

---
+++ front-end/src/pages/CustomerProducts.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CustomerNavBar from '../components/CustomerNavBar';
+
+function CustomerProducts() {
+  return (
+    <div>
+      <CustomerNavBar />
+      Customer Products
+    </div>
+  );
+}
+
+export default CustomerProducts;
