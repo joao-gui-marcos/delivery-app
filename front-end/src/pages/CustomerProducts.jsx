f462adba8358d84e0d492087bff477a4fc4e757d
Joao Guilherme <jguilherme.marcos@gmail.com>
2023-03-01 16:48:33 -0300

    feat: implement productCard and CustomerProduct components

--- front-end/src/pages/CustomerProducts.jsx
+++ front-end/src/pages/CustomerProducts.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import CustomerNavBar from '../components/CustomerNavBar';
+import ProductCard from '../components/ProductCard';
 
 function CustomerProducts() {
   return (
     <div>
       <CustomerNavBar />
       Customer Products
+      <ProductCard />
     </div>
   );
 }
