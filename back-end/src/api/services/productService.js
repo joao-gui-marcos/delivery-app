45dd4ac25e2175cf9bafe5322ee6c2ef18321334
João Victor Braga <joao.victtor07@hotmail.com>
2023-03-01 16:15:37 -0300

    chore: arruma erro no lint

--- back-end/src/api/services/productService.js
+++ back-end/src/api/services/productService.js
@@ -3,7 +3,7 @@ const { Product } = require('../../database/models');
 const getAllProducts = async () => {
   const allProducts = await Product.findAll({});
 
-  return { statusCode: 200, data: allProducts }
+  return { statusCode: 200, data: allProducts };
 };
 
 module.exports = {
