b9b3bc4766f6a03c6391a22117b2aec82f3d2baf
Joao Guilherme <jguilherme.marcos@gmail.com>
2023-03-02 18:22:27 -0300

    feat: implement cart logic to save user products and display total price

--- front-end/src/components/ProductCard.jsx
+++ front-end/src/components/ProductCard.jsx
@@ -1,17 +1,41 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+// import Cart from '../entities/cart';
 
-function ProductCard({ name, price, image, id }) {
-  const [quantity, setQuantity] = useState(0);
+function ProductCard({ name, price, image, id, onUpdateCart }) {
+  // const [quantity, setQuantity] = useState(0);
+  const [quantity, setQuantity] = useState(
+    JSON.parse(localStorage.getItem('cart'))?.find((item) => item
+      .id === id)?.quantity || 0,
+  );
 
   const handleIncreaseQuantity = () => {
+    // const newQuantity = quantity + 1;
+    // setQuantity(newQuantity);
+    // Cart.addItem({ id, name, price, quantity: newQuantity });
     setQuantity(quantity + 1);
+    onUpdateCart({ id, name, price, quantity: quantity + 1 });
   };
 
   const handleDecreaseQuantity = () => {
+    // if (quantity > 0) {
+    //   const newQuantity = quantity - 1;
+    //   setQuantity(newQuantity);
+    //   Cart.addItem({ id, name, price, quantity: newQuantity });
+    // }
     if (quantity > 0) {
       setQuantity(quantity - 1);
+      onUpdateCart({ id, name, price, quantity: quantity - 1 });
     }
+  };
+
+  const handleQuantityChange = ({ target: { value } }) => {
+    // const newQuantity = parseInt(value, 10);
+    // setQuantity(newQuantity);
+    // Cart.addItem({ id, name, price, quantity: newQuantity });
+    const newQuantity = parseInt(value, 10);
+    setQuantity(newQuantity);
+    onUpdateCart({ id, name, price, quantity: newQuantity });
   };
 
   return (
@@ -33,7 +57,7 @@ function ProductCard({ name, price, image, id }) {
       />
       <div>
         <button
-          data-testid={ `customer_products__button-card-add-item-${id}` }
+          data-testid={ `customer_products__button-card-rm-item-${id}` }
           type="button"
           onClick={ handleDecreaseQuantity }
         >
@@ -43,9 +67,10 @@ function ProductCard({ name, price, image, id }) {
           data-testid={ `customer_products__input-card-quantity-${id}` }
           type="number"
           value={ quantity }
+          onChange={ handleQuantityChange }
         />
         <button
-          data-testid={ `customer_products__button-card-rm-item-${id}` }
+          data-testid={ `customer_products__button-card-add-item-${id}` }
           type="button"
           onClick={ handleIncreaseQuantity }
         >
@@ -58,9 +83,10 @@ function ProductCard({ name, price, image, id }) {
 
 ProductCard.propTypes = {
   name: PropTypes.string.isRequired,
-  price: PropTypes.number.isRequired,
+  price: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
   id: PropTypes.number.isRequired,
+  onUpdateCart: PropTypes.func.isRequired,
 };
 
 export default ProductCard;
--- front-end/src/pages/CustomerProducts.jsx
+++ front-end/src/pages/CustomerProducts.jsx
@@ -1,10 +1,17 @@
 import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import CustomerNavBar from '../components/CustomerNavBar';
 import ProductCard from '../components/ProductCard';
+// import CartTotalButton from '../components/CartTotalButton';
 
 function CustomerProducts() {
+  const history = useHistory();
   const [products, setProducts] = useState([]);
   const userData = localStorage.getItem('user');
+  const [cart, setCart] = useState(JSON.parse(localStorage.getItem('cart')) || []);
+  const [cartVersion, setCartVersion] = useState(0);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const INDEX = -1;
 
   useEffect(() => {
     fetch('http://localhost:3001/products', {
@@ -18,6 +25,38 @@ function CustomerProducts() {
       .catch((error) => console.error('Error fetching products', error));
   }, [userData]);
 
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cart));
+    // setTotalPrice(
+    //   cart.reduce((total, item) => total
+    //   + (item.quantity * parseFloat(item.price.replace('.', ','))), 0)
+    //     .toLocaleString('pt-BR', { minimumFractionDigits: 2 }),
+    // );
+    const tot = cart.reduce((total, item) => total
+     + (item.quantity * parseFloat(item.price)), 0).toFixed(2);
+
+    const formattedTotal = Number(tot)
+      .toLocaleString('pt-BR', { minimumFractionDigits: 2 });
+
+    setTotalPrice(formattedTotal);
+  }, [cart]);
+
+  const updateCartItem = (productId, { id, name, price, quantity }) => {
+    const itemIndex = cart.findIndex((item) => item.id === productId);
+    if (itemIndex === INDEX) {
+      setCart([...cart, { id, name, price, quantity }]);
+    } else {
+      const updatedCart = [...cart];
+      updatedCart[itemIndex].quantity = quantity;
+      setCart(updatedCart);
+    }
+    setCartVersion(cartVersion + 1);
+  };
+
+  const handleClick = () => {
+    history.push('/customer/checkout');
+  };
+
   return (
     <div>
       <CustomerNavBar name={ JSON.parse(userData).name } />
@@ -28,8 +67,22 @@ function CustomerProducts() {
           price={ product.price }
           image={ product.urlImage }
           id={ product.id }
+          onUpdateCart={ (quantity) => updateCartItem(product.id, quantity) }
         />
       ))}
+      <button
+        data-testid="customer_products__button-cart"
+        type="button"
+        onClick={ handleClick }
+        disabled={ totalPrice <= '0,00' }
+      >
+        Total: $
+        <span
+          data-testid="customer_products__checkout-bottom-value"
+        >
+          {totalPrice}
+        </span>
+      </button>
     </div>
   );
 }
