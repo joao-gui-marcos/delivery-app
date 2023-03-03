68abc9da65207844989107a75f783e8895b57216
Kevillin Santos <kevillin_santos@hotmail.com>
2023-03-03 16:43:47 -0300

    Cria tabela para pegar os dados da tabela sales pelo Id do usuário, cria rota para atualizar dados de pendente para preparando ou entregue

--- back-end/src/api/controllers/checkoutController.js
+++ back-end/src/api/controllers/checkoutController.js
@@ -1,9 +1,10 @@
-const CustomerService = require('../services/checkoutService');
+const checkoutService = require('../services/checkoutService');
+const { validateToken } = require('../services/utils/validadeJWT');
 
 const createOrder = async (req, res) => {
   const order = req.body;
 
-  const { statusCode, data } = await CustomerService.createOrder(order);
+  const { statusCode, data } = await checkoutService.createOrder(order);
 
   return res.status(statusCode).json(data);
 };
@@ -11,7 +12,26 @@ const createOrder = async (req, res) => {
 const findOrderById = async (req, res) => {
   const { id } = req.params;
 
-  const { statusCode, data } = await CustomerService.findOrderById(id);
+  const { statusCode, data } = await checkoutService.findOrderById(id);
+
+  return res.status(statusCode).json(data);
+};
+
+const updateOrder = async (req, res) => {
+  const { id } = req.params;
+  const { status } = req.body;
+
+  const { statusCode, data } = await checkoutService.updateOrder(id, status);
+
+  return res.status(statusCode).json(data);
+};
+
+const requestSaleInformationFromIdCustomer = async (req, res) => {
+  const token = req.headers.authorization;
+
+  const { id } = validateToken(token);
+
+  const { statusCode, data } = await checkoutService.requestSaleInformationFromIdCustomer(id);
 
   return res.status(statusCode).json(data);
 };
@@ -19,4 +39,6 @@ const findOrderById = async (req, res) => {
 module.exports = {
   createOrder,
   findOrderById,
+  updateOrder,
+  requestSaleInformationFromIdCustomer,
 };
