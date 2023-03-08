0e8c563af85f6928462870ddc1964982f3c7544e
Joao Guilherme <jguilherme.marcos@gmail.com>
2023-03-08 16:49:33 -0300

    feat: implement seller order details screen

--- front-end/src/pages/SellerOrderDetail.jsx
+++ front-end/src/pages/SellerOrderDetail.jsx
@@ -1,7 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import CustomerNavBar from '../components/CustomerNavBar';
+import SellerOrderDetailsHeader from '../components/seller/SellerOrderDetailsHeader';
+import SellerOrderDetailsTable from '../components/seller/SellerOrderDetailsTable';
 
 function SellerOrderDetail() {
-  return <div>Loading...</div>;
+  const { id } = useParams();
+  const userData = localStorage.getItem('user');
+  const [order, setOrder] = useState(null);
+
+  useEffect(() => {
+    const fetchOrders = () => {
+      fetch(`http://localhost:3001/checkout/order/${id}`, {
+        method: 'GET',
+        headers: {
+          Authorization: JSON.parse(userData).token,
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => setOrder(data))
+        .catch((error) => console.error('Error fetching orders', error));
+    };
+    fetchOrders();
+  }, [userData]);
+
+  if (!order) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <CustomerNavBar name={ JSON.parse(userData).name } />
+      <SellerOrderDetailsHeader
+        orderId={ order.id }
+        saleDate={ order.saleDate }
+        status={ order.status }
+      />
+      <SellerOrderDetailsTable items={ order.products } />
+      <p
+        type="button"
+        data-testid="seller_order_details__element-order-total-price"
+      >
+        Total Price: R$
+        {' '}
+        <span>
+          {Number(order.totalPrice)
+            .toLocaleString('pt-BR', { minimumFractionDigits: 2 })}
+        </span>
+      </p>
+    </div>
+  );
 }
 
 export default SellerOrderDetail;
